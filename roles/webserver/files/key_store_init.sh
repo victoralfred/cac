c02452353285118f2a4b507409f96a162839e1dd
victoralfred <victoralfred32@gmail.com>
2024-10-28 22:14:50 +0100

    Added timeout to except command

--- roles/webserver/files/key_store_init.sh
+++ roles/webserver/files/key_store_init.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-
+set -x
 # Run an expect command to automate prompts in initcert.js
 expect -c "
-    # Start the initcert.js script in a new shell
-    spawn bash -c \"./initcert.js\"
+  # Start the initcert.js script in a new shell
+  spawn bash -c \"./initcert.js\"
 
-    # Respond to the initial passphrase prompt
-    expect -exact \"Enter passphrase of certificate for sample: \"
-    send \"password\n\"
+  # Respond to the initial passphrase prompt with a timeout
+  expect -timeout 10 -exact \"Enter passphrase of certificate for sample: \"
+  send \"password\n\"
 
-    # Respond to the password prompt
-    expect -exact \"Password: \"
-    send \"password\n\"
+  # Respond to the password prompt with a timeout
+  expect -timeout 10 -exact \"Password: \"
+  send \"password\n\"
 
-    # Allows interaction afterward, if needed
-    interact
+  # Wait for the script to finish or a timeout
+  expect -timeout 30 eof
 "
